e6004bc6b1ceb44d38594f95fc05f03f400907bf
RigOmoTis <93663627+RigOmoTis@users.noreply.github.com>
2022-11-09 15:52:50 +0700

    Update 4.6.3.py

--- Операторы и выражения в Python/4.6.3.py
+++ Операторы и выражения в Python/4.6.3.py
@@ -1,7 +1,7 @@
 from random import random, randint
 
 x = randint(0, 99999999999) + random()
-print(f'x = {x}', '\n1) {0:.2f}'.format(x), '\n2)', round(x), '\n3)', end=' ')
+print(f'x = {x}', '\n') {0:.2f}'.format(x), '\n2)', round(x), '\n3)', end=' ')
 i = 100000000000 // x
 while (i // 10) >= 1:
     print(0, end='')
