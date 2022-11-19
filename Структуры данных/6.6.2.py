006249ce5f29be3355d13209d815bbc9742192b8
RigOmoTis <93663627+RigOmoTis@users.noreply.github.com>
2022-11-19 15:55:06 +0700

    Update 6.6.2.py

--- Структуры данных/6.6.2.py
+++ Структуры данных/6.6.2.py
@@ -1,18 +1,18 @@
 import random
 
 n = 5               ### Размер списка
-L = []              ### Пустой список
+list = []           ### Пустой список
 x = 0               ### Переменная для хранения результата
 max = -100          ### Переменная для хранения максимального элемента из списка
 min = 100           ### Переменная для хранения минимального элемента из списка
 
 
 for i in range(n):
-    L.append(random.randint(-99, 99))
-    if L[i] < min:
-        min = L[i]
-    if L[i] > max:
-        max = L[i]
+    list.append(random.randint(-99, 99))
+    if list[i] < min:
+        min = list[i]
+    if list[i] > max:
+        max = list[i]
 print( end='')
 x = max - min
-print('elements =', L ,', result: ', x)
+print('elements =', list, ', result: ', x)
