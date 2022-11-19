ea2b7820f5d56e6b2013530705d8c45e37f4fd82
RigOmoTis <93663627+RigOmoTis@users.noreply.github.com>
2022-11-19 15:55:59 +0700

    Update 6.6.1.py

--- Структуры данных/6.6.1.py
+++ Структуры данных/6.6.1.py
@@ -1,17 +1,17 @@
 import random
 
 n = 5           ### Количество элементов списка
-L = []          ### Пустой список
+list = []       ### Пустой список
 x = 0           ### Переменная для хранения результата
 
 print("elements = [", end='')
 for i in range(n):
-    L.append(random.randint(0, 10))
+    list.append(random.randint(0, 10))
     if i < n - 1:
-        print(L[i], end=', ')
+        print(list[i], end=', ')
     else:
-        print(L[i],end='], ')
+        print(list[i], end='], ')
     if i % 2 == 0:
-        x += L[i]
-x *= L[n - 1]
+        x += list[i]
+x *= list[n - 1]
 print('result:', x)
